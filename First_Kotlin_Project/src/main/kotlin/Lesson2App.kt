61f940987a26810f96994cd676af2560bd5f705b
Trenton Weir <81660030+TrentonWeir@users.noreply.github.com>
2023-08-25 16:43:28 -0400

    Think I am done with M02, need to check though

---
+++ First_Kotlin_Project/src/main/kotlin/Lesson2App.kt
@@ -0,0 +1,29 @@
+fun lesson2() {
+    // First Loop
+    loop(10) { index ->
+        println("Loop 1 Current Index: $index")
+    }
+    println("We have exited the first loop")
+
+    // Second Loop
+    var exited = false
+    loop(10) { index ->
+        if (!exited) {
+            println("Loop 2: Current Index $index")
+            if (index >= 5) {
+                exited = true
+                return@loop
+            }
+        }
+    }
+    println("We have exited the second loop")
+
+    println("The application is complete...")
+}
+
+fun loop(numberOfIntervals: Int = 10, callback: (Int) -> Unit = {}) {
+    for (i in 1..numberOfIntervals) {
+        callback(i)
+    }
+}
+
---
+++ First_Kotlin_Project/src/main/kotlin/Student.kt
@@ -0,0 +1,10 @@
+open class Student(
+    val firstName: String,
+    val lastName: String,
+    val streetAddress: String,
+    val city: String,
+    val state: String,
+    val zip: Int,
+    val gpa: Double,
+    val major: String
+)
